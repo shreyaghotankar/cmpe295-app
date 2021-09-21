7fc09ef534ce8ac9e18c927cd0078b658f48ba51
Anastasia Zimina <anastasba@gmail.com>
2021-09-21 01:41:06 -0700

    adding hooks

--- src/shared/hooks/items.js
+++ src/shared/hooks/items.js
@@ -2,7 +2,7 @@ const getItems = function() {
     return new Promise(function(resolve) {
       setTimeout(function(){
           console.log("Items read from DB")
-          // returning empty list of items
+          // returning  list of items
         resolve(["h"])
       }, 1000);
     });
