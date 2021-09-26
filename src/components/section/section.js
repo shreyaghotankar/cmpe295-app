661c998df2a0ac200c2b1408642be58c76e72347
Anastasia Zimina <anastasba@gmail.com>
2021-09-26 12:25:14 -0700

    section header set up finished

--- src/components/section/section.js
+++ src/components/section/section.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import PropTypes from 'prop-types';
 import { SECTIONS } from "../../shared/constants";
 import styles from './section.module.scss';
@@ -10,10 +10,22 @@ function Section (props) {
         children
     } = props;
 
+    const [icon, setIcon] = useState(null)
+
+    useEffect(() => {
+        if (section === SECTIONS.CLOSET) {
+            setIcon(<HangerIcon />);
+        } else if (section === SECTIONS.OUTFITS) {
+            setIcon(null);
+        }
+    }, [section])
+
     return (
         <div className={styles.container}>
-            <div className={styles.header}>{section}</div>
-            
+            <div className={styles.header}>
+                <span className={styles.icon}>{icon}</span>
+                <span>{section}</span>
+            </div>
             <div>
                 {children}
             </div>
