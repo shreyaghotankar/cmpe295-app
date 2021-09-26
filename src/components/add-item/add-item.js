cd42cf9435554fa4de3513001a5f8ae7a883cad1
Anastasia Zimina <anastasba@gmail.com>
2021-09-25 23:41:48 -0700

    adding component

---
+++ src/components/add-item/add-item.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import PropTypes from 'prop-types';
+import styles from './add-item.module.scss';
+
+function AddItem () {
+    return (
+        <div>Add Item Will be here</div>
+    )
+}
+
+AddItem.propTypes = {
+
+}
+
+export default AddItem;
--- src/components/welcome-modal/welcome-modal.js
+++ src/components/welcome-modal/welcome-modal.js
@@ -6,6 +6,7 @@ import styles from './welcome-modal.module.scss';
 import CustomModal from "../custom-modal/custom-modal";
 import EmptyCloset from "./components/empty-closet/empty-closet";
 import EmptyColor from "./components/empty-color/empty-color";
+import AddItem from "../add-item/add-item";
 
 function WelcomeModal (props) {
     const {
@@ -19,7 +20,7 @@ function WelcomeModal (props) {
         <CustomModal show={show} onHide={onHide}>
             {currentStep === WELCOME_STEPS.CLOSET && <EmptyCloset onClick={() => setCurrentStep(WELCOME_STEPS.NEW_ITEM)}/>}
             {currentStep === WELCOME_STEPS.COLORS && <EmptyColor />}
-            {currentStep === WELCOME_STEPS.NEW_ITEM && <div>Add new item here</div>}
+            {currentStep === WELCOME_STEPS.NEW_ITEM && <AddItem />}
             {currentStep === WELCOME_STEPS.CLOSET && 
             <Button variant="link" onClick={()=>{setCurrentStep(WELCOME_STEPS.COLORS)}}>Skip this step
             </Button>}
