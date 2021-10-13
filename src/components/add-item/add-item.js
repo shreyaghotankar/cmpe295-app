aa4d1ee49991021204545b5164dc46ba63cabd59
Shreya Ghotankar <70411701+shreyaghotankar@users.noreply.github.com>
2021-10-13 15:47:03 -0700

    S3 put

--- src/components/add-item/add-item.js
+++ src/components/add-item/add-item.js
@@ -1,10 +1,49 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from 'prop-types';
 import styles from './add-item.module.scss';
+import {Row, Col, Form, Button} from 'react-bootstrap';
+import { Storage } from "aws-amplify";
+
+Storage.configure({ level: 'private' });
 
 function AddItem () {
+    const [images, setImages] = useState([]);
+
+    useEffect(() => {
+        fetchImages()
+    }, [])
+
+
+    async function fetchImages() {
+        let imageKeys = await Storage.list('')
+        imageKeys = await Promise.all(imageKeys.map(async k => {
+        const signedUrl = await Storage.get(k.key)
+        return signedUrl
+        }))
+        console.log('imageKeys: ', imageKeys)
+        setImages(imageKeys)
+    }  
+
+    async  function onchange(e){
+        const file = e.target.files[0];
+        const result = await Storage.put(file.name, file)
+        console.log("results: ", result)
+        fetchImages()
+
+        }
+
     return (
-        <div>Add Item Will be here</div>
+        <div>Add Item Will be here
+        <div>
+        
+        <form>
+            <label>Upload Image:
+                <div>
+                <input type='file' onChange={onchange} /></div>
+            </label>
+        </form>
+        </div>
+        </div>
     )
 }
 
