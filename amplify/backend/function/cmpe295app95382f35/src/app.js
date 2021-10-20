80a8fb354ed16feef3e681a371981e7cd882c491
Shreya Ghotankar <70411701+shreyaghotankar@users.noreply.github.com>
2021-10-20 15:33:15 -0700

    put item to s3 and dynamodb

--- amplify/backend/function/cmpe295app95382f35/src/app.js
+++ amplify/backend/function/cmpe295app95382f35/src/app.js
@@ -13,6 +13,7 @@ var awsServerlessExpressMiddleware = require('aws-serverless-express/middleware'
 var bodyParser = require('body-parser')
 var express = require('express')
 
+
 AWS.config.update({ region: process.env.TABLE_REGION });
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
@@ -22,7 +23,7 @@ if(process.env.ENV && process.env.ENV !== "NONE") {
   tableName = tableName + '-' + process.env.ENV;
 }
 
-const userIdPresent = false; // TODO: update in case is required to use that definition
+const userIdPresent = true; // TODO: update in case is required to use that definition
 const partitionKeyName = "imageId";
 const partitionKeyType = "S";
 const sortKeyName = "";
--- src/components/add-item/add-item.js
+++ src/components/add-item/add-item.js
@@ -2,12 +2,18 @@ import React, { useEffect, useState } from "react";
 import PropTypes from 'prop-types';
 import styles from './add-item.module.scss';
 import {Row, Col, Form, Button} from 'react-bootstrap';
-import { Storage } from "aws-amplify";
+import { API, Storage } from "aws-amplify";
+
 
 Storage.configure({ level: 'private' });
 
+let apiName = 'dBApi';
+let path = '/images';
+
 function AddItem () {
     const [images, setImages] = useState([]);
+    const[attributes, setAttributes] = useState([])
+    const[image_id, setImage_id] = useState("");
 
     useEffect(() => {
         fetchImages()
@@ -24,24 +30,62 @@ function AddItem () {
         setImages(imageKeys)
     }  
 
-    async  function onchange(e){
+    async function onchange(e){
         const file = e.target.files[0];
         const result = await Storage.put(file.name, file)
+        const image_id = result.key
+        //await Storage.get(result.key)
         console.log("results: ", result)
+        console.log("image id: ", image_id)
+        setImage_id(image_id);
         fetchImages()
+
+        }
+        const onBoxChecked = e =>{
+            const target = e.target;
+            const attributes = target.value;
+            setAttributes(attributes)
+          }
+        const handleSubmit = e => {
+            e.preventDefault();
+            let item = {image_id, attributes};
+            console.log("payload:", item)
+            API.put(apiName,path, {
+               body: {
+                   imageId: image_id,
+                   attributes: attributes
+               },
+           }).then(res => {
+               console.log("Database updated");
+           })
+           .catch(error => {
+               console.log(error.response)
+           });
 
         }
 
     return (
         <div>Add Item Will be here
         <div>
-        
         <form>
             <label>Upload Image:
                 <div>
-                <input type='file' onChange={onchange} /></div>
+                <input type="file" onChange={onchange} />
+                </div>
             </label>
         </form>
+        </div>
+        <div>
+        <form onSubmit={handleSubmit}>
+            <label>Select Clothing Type:</label>
+            <div/>
+              <input type="radio" onChange={onBoxChecked} value="top" name="clothingType"/>
+              <label for="top">Top</label>
+              <div></div><div></div>
+              <input type="radio" onChange={onBoxChecked} value="bottom" name="clothingType"/>
+              <label for="bottom">Bottom</label>
+              <button>Submit</button>
+          </form>
         </div>
         </div>
     )
